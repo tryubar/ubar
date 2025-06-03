915f4bb1a24437a1d02be9311b1426c9dbda6eb8
Ranjan Biswas <rkb57@georgetown.edu>
2025-06-03 11:31:26 -0400

    new project

---
+++ ubar/ubar/ubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ubarApp.swift
+//  ubar
+//
+//  Created by Ranjan Biswas on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ubar/ubarTests/ubarTests.swift
@@ -0,0 +1,16 @@
+//
+//  ubarTests.swift
+//  ubarTests
+//
+//  Created by Ranjan Biswas on 6/3/25.
+//
+
+import Testing
+
+struct ubarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
