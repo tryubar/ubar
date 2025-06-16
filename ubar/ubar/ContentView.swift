2661753e0cb49e5d1012bd3024960b4df14287ea
Ranjan Biswas <rkb57@georgetown.edu>
2025-06-16 17:24:39 -0400

    changes

--- ubar/ubar/ContentView.swift
+++ ubar/ubar/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Color(.black).ignoresSafeArea(edges: .all)
+            Image("margarita").resizable().cornerRadius(40).aspectRatio(contentMode: .fit).padding(.all)
+            Text("UBar - Drink away").font(.title).fontWeight(.bold).fontWeight(.semibold)
         }
-        .padding()
+       
+        
     }
 }
 
